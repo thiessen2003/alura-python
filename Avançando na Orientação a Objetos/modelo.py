68eecbf9506c50eedf730b1df782f49f2920b9d0
thiessen2003 <thiessengabriel@gmail.com>
2023-04-23 22:28:11 -0300

    Daily Commit

--- Avançando na Orientação a Objetos/modelo.py
+++ Avançando na Orientação a Objetos/modelo.py
@@ -1,21 +1,34 @@
 class Filme:
 
     def __init__(self, nome, ano, duracao):
-        self.nome = nome.title()
+        self.__nome = nome.title()
         self.ano = nome
         self.duracao = duracao
-        self.likes = 0
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
 
     def dar_like(self):
-        self.likes += 1
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def set_nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
 
 class Serie:
 
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome
+        self.__ome = nome
         self.ano = nome
         self.temporadas = temporadas
-        self.likes = 0
+        self.__likes = 0
 
     def dar_like(self):
         self.likes += 1
