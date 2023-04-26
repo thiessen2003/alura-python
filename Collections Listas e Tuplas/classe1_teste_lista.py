6b570a0fa006c604c4b92242b43884aeea79e2e4
thiessen2003 <thiessengabriel@gmail.com>
2023-04-26 13:21:39 -0300

    Daily Commit

--- Collections Listas e Tuplas/classe1_teste_lista.py
+++ Collections Listas e Tuplas/classe1_teste_lista.py
@@ -9,9 +9,14 @@ class ContaCorrete:
     def __str__(self):
         return (f"[>>>Código {self.codigo} Saldo {self.saldo}<<<]")
 
+def deposita_para_todas(contas):
+    for conta in contas:
+        conta.deposita(100)
+
 conta_do_gui = ContaCorrete(15)
 conta_da_dani = ContaCorrete(47855)
 contas = [conta_da_dani, conta_do_gui]
 
 for conta in contas:
     print(conta)
+
---
+++ Collections Listas e Tuplas/tupla1_teste.py
@@ -0,0 +1,7 @@
+#BASIC NOTES: Tuplas são imutáveis, entretanto
+#podemos inseri-las em listas | somente seus valores internos são imutáveis
+guilherme = ('Guilherme', 37, 1981)
+daniela = ('Daniela', 31, 1987)
+
+usuarios = [guilherme, daniela]
+
