ca426e8cda4ecbc979c29dcc88467d8e92cc6812
thiessen2003 <thiessengabriel@gmail.com>
2023-04-26 09:31:45 -0300

    Daily Commit

--- Collections Listas e Tuplas/classe1.py
+++ Collections Listas e Tuplas/classe1.py
@@ -8,3 +8,10 @@ class ContaCorrete:
 
     def __str__(self):
         return (f"[>>>Código {self.codigo} Saldo {self.saldo}<<<]")
+
+conta_do_gui = ContaCorrete(15)
+conta_da_dani = ContaCorrete(47855)
+contas = [conta_da_dani, conta_do_gui]
+
+for conta in contas:
+    print(conta)
