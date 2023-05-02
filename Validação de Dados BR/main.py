0bdfe2fb37021abef3609abd349d79a0fc9717dc
thiessen2003 <thiessengabriel@gmail.com>
2023-05-02 09:31:08 -0300

    Daily Commit

--- Validação de Dados BR/main.py
+++ Validação de Dados BR/main.py
@@ -1,6 +1,10 @@
 from cpf import CPF
+from validate_docbr import CPF
+from validate_docbr import CNPJ
 
-cpf = "15389880058"
-objeto_cpf = CPF(cpf)
+exemplo_cpnj = "13254678410913"
+cnpj = CNPJ()
+cpf = CPF()
+print(cpf.validate("12345123454"))
+print(cnpj.validate(exemplo_cpnj))
 
-print(objeto_cpf)
